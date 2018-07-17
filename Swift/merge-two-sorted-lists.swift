3311ffd902610b4f46ae3de839f1fd59733dc5e8
heyuze <ryzehe@gmail.com>
2018-07-17 16:25:11 +0800

    add merge-two-sorted-lists.swift

---
+++ Swift/merge-two-sorted-lists.swift
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.next = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func mergeTwoLists(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+        let l = ListNode(0)
+        var node = l
+        var l1 = l1, l2 = l2
+        
+        while l1 != nil && l2 != nil {
+            if l1!.val < l2!.val {
+                node.next = l1
+                l1 = l1!.next
+            } else {
+                node.next = l2
+                l2 = l2!.next
+            }
+            node = node.next!
+        }
+        
+        node.next = l1 ?? l2
+        return l.next
+    }
+}
