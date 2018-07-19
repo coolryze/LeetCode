5703cdccda737ca2eabcbccc54dbb2f030e35d67
heyuze <ryzehe@gmail.com>
2018-07-19 22:12:49 +0800

    add implement-strstr.py

---
+++ Python/implement-strstr.py
@@ -0,0 +1,27 @@
+class Solution:
+    def strStr1(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        return haystack.find(needle)
+
+    def strStr2(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        hLen = len(haystack)
+        nLen = len(needle)
+        
+        if nLen == 0:
+            return 0
+        if hLen < nLen:
+            return -1
+
+        for i in range(hLen - nLen + 1):
+            if haystack[i:i+nLen] == needle:
+                return i
+        return -1
