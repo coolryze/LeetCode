98d7faa979232cff36462e3430c082bbf42a08ac
heyuze <ryzehe@gmail.com>
2018-07-20 19:35:27 +0800

    add search-insert-position.py

---
+++ Python/search-insert-position.py
@@ -0,0 +1,17 @@
+class Solution:
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+          mid = left + (right - left) // 2
+          if nums[mid] >= target:
+            right = mid - 1
+          else:
+            left = mid + 1
+        return left
