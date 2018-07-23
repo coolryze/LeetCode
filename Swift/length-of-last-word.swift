8c6c7a23d95c12531f2de42f984b389d873362b7
heyuze <ryzehe@gmail.com>
2018-07-24 00:29:40 +0800

    update length-of-last-word.swift

--- Swift/length-of-last-word.swift
+++ Swift/length-of-last-word.swift
@@ -1,5 +1,21 @@
 class Solution {
-    func lengthOfLastWord(_ s: String) -> Int {
+    func lengthOfLastWord1(_ s: String) -> Int {
+        var res = 0
+        
+        for char in s.reversed() {
+            if char == " " {
+                if res != 0 {
+                    break
+                }
+            } else {
+                res += 1
+            }
+        }
+        
+        return res
+    }
+    
+    func lengthOfLastWord2(_ s: String) -> Int {
         let words = s.split(separator: " ")
         return words.last?.count ?? 0
     }
