0dd54b159c4f74d905a54476b7981541fafcba41
heyuze <ryzehe@gmail.com>
2018-07-24 23:59:44 +0800

    add plus-one.swift

---
+++ Swift/plus-one.swift
@@ -0,0 +1,19 @@
+class Solution {
+    func plusOne(_ digits: [Int]) -> [Int] {
+        var digits = digits
+        var index = digits.count - 1
+        
+        while index >= 0 {
+            if digits[index] < 9 {
+                digits[index] += 1
+                return digits
+            }
+            
+            digits[index] = 0
+            index -= 1
+        }
+        
+        digits.insert(1, at: 0)
+        return digits
+    }
+}
