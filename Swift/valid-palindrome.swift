60500e55500a086e924f31381c875012d0c5a638
heyuze <ryzehe@gmail.com>
2018-08-01 23:59:42 +0800

    add valid-palindrome.swift

---
+++ Swift/valid-palindrome.swift
@@ -0,0 +1,17 @@
+class ValidPalindrome {
+    func isPalindrome(_ s: String) -> Bool {
+        let characters = s.lowercased().characters
+        
+        let cleaned = characters.filter { character in
+            return character.description.rangeOfCharacter(from: CharacterSet.alphanumerics) != nil
+        }
+        
+        let total = cleaned.count
+        for i in 0 ..< total/2 {
+            if cleaned[i] != cleaned[total - 1 - i] {
+                return false
+            }
+        }
+        return true
+    }
+}
