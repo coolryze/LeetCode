6857ef4499fee8eae2a429d215cf87c02fff7e17
heyuze <ryzehe@gmail.com>
2018-08-09 23:36:08 +0800

    add reverse-bits.py

---
+++ Python/reverse-bits.py
@@ -0,0 +1,12 @@
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    def reverseBits(self, n):
+        result = 0
+
+        for _ in range(32):
+            result <<= 1
+            result |= n & 1
+            n >>= 1
+
+        return result
