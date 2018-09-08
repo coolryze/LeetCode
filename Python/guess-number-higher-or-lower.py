d47fa65efba2c81447f8de9e11fbf35789833ede
heyuze <ryzehe@gmail.com>
2018-09-08 23:01:37 +0800

    add guess-number-higher-or-lower.py

---
+++ Python/guess-number-higher-or-lower.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def guessNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        left = 1
+        right = n
+
+        while left <= right:
+            mid = left + (right - left) // 2
+            res = guess(mid)
+            if res == 0:
+                return mid
+            elif res == 1:
+                right = mid - 1
+            elif res == -1:
+                left = mid + 1
