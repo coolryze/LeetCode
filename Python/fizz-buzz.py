06f1fb27d2925964fc316eb5c788b234773a1fe9
heyuze <ryzehe@gmail.com>
2018-09-12 23:10:58 +0800

    add fizz-buzz.py

---
+++ Python/fizz-buzz.py
@@ -0,0 +1,22 @@
+class Solution:
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        res = list()
+
+        if n < 0:
+            return res
+
+        for i in range(1, n + 1):
+            if i % 3 == 0 and i % 5 == 0:
+                res.append('FizzBuzz')
+            elif i % 3 == 0:
+                res.append('Fizz')
+            elif i % 5 == 0:
+                res.append("Buzz")
+            else:
+                res.append(str(i))
+
+        return res
