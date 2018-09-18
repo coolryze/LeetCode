21d57ad1d913217df8a39442646fc09f6c352bfd
heyuze <ryzehe@gmail.com>
2018-09-19 00:11:29 +0800

    update add-strings.swift

--- Swift/add-strings.swift
+++ Swift/add-strings.swift
@@ -1,7 +1,7 @@
 class Solution {
-    func addStrings(_ num1: String, _ num2: String) -> String {
-        var chars1 = [Character](num1)
-        var chars2 = [Character](num2)
+    func addStrings1(_ num1: String, _ num2: String) -> String {
+        var chars1 = Array(num1)
+        var chars2 = Array(num2)
 
         while chars1.count != chars2.count {
             if chars1.count > chars2.count {
@@ -18,13 +18,43 @@ class Solution {
         for i in (0..<chars1.count).reversed() {
             let num1 = chars1[i].unicodeScalars.first!.value - zeroValue
             let num2 = chars2[i].unicodeScalars.first!.value - zeroValue
-            var x = num1 + num2
-            x += carry
-            let y = x % 10
-            carry = x / 10
-            resChars.insert(Character(UnicodeScalar(y + zeroValue)!), at: 0)
+            var sum = num1 + num2 + carry
+            carry = sum / 10
+            sum = sum % 10
+            resChars.insert(Character(UnicodeScalar(sum + zeroValue)!), at: 0)
+        }
+
+        if carry != 0 {
+            resChars.insert(Character(UnicodeScalar(carry + zeroValue)!), at: 0)
         }
 
         return String(resChars)
+    }
+
+    func addStrings2(_ num1: String, _ num2: String) -> String {
+        let chars1 = Array(num1.reversed())
+        let chars2 = Array(num2.reversed())
+        var i = 0, j = 0, sum = 0, carry = 0
+        var res = ""
+
+        while i < chars1.count || j < chars2.count || carry != 0 {
+            sum = carry
+
+            if i < chars1.count {
+                sum += Int(String(chars1[i]))!
+                i += 1
+            }
+            if j < chars2.count {
+                sum += Int(String(chars2[j]))!
+                j += 1
+            }
+
+            carry = sum / 10
+            sum = sum % 10
+
+            res.append(String(sum))
+        }
+
+        return String(res.reversed())
     }
 }
