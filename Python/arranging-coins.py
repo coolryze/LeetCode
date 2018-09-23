84146404e579f437d6d82acf59fa792fb1b82cec
heyuze <ryzehe@gmail.com>
2018-09-24 00:34:48 +0800

    update arranging-coins.py

--- Python/arranging-coins.py
+++ Python/arranging-coins.py
@@ -5,10 +5,12 @@ class Solution:
         :rtype: int
         """
         left, right = 1, n
+
         while left <= right:
             mid = left + (right - left) // 2
-            if 2 * n < mid * (mid+1):
+            if n * 2 < mid * (mid + 1):
                 right = mid - 1
             else:
                 left = mid + 1
+
         return left - 1
