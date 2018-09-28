17122336dffd7eb3389e03d2047af35fe9da5e20
heyuze <ryzehe@gmail.com>
2018-09-29 00:51:58 +0800

    add repeated-substring-pattern.py

---
+++ Python/repeated-substring-pattern.py
@@ -0,0 +1,8 @@
+class Solution:
+    def repeatedSubstringPattern(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        ss = (s + s)[1:-1]
+        return ss.find(s) != -1
