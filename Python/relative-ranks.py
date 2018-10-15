c0144f1f1ab5ebd46c89f3d00c2978c301c8641b
heyuze <ryzehe@gmail.com>
2018-10-15 21:45:24 +0800

    add relative-ranks.py

---
+++ Python/relative-ranks.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def findRelativeRanks(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[str]
+        """
+        sortedNums = sorted(nums)[::-1]
+        ranks = ['Gold Medal', 'Silver Medal', 'Bronze Medal']
+        ranks = ranks + list(map(str, range(4, len(nums) + 1))) if len(nums) > 3 else ranks
+        return list(map(dict(zip(sortedNums, ranks)).get, nums))
