ae47bf3b40c68e1b874020b9321658d2aaecec2e
heyuze <ryzehe@gmail.com>
2018-10-23 22:09:41 +0800

    add student-attendance-record-i.py

---
+++ Python/student-attendance-record-i.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def checkRecord(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        countA = 0
+
+        for i in range(len(s)):
+            if s[i] == 'A':
+                countA += 1
+                if countA == 2:
+                    return False
+            if i < len(s) - 2 and s[i] == s[i+1] == s[i+2] == 'L':
+                return False
+
+        return True
