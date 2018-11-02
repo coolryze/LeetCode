2ec41023cb0af469e178602399eb557c1117fd3e
heyuze <ryzehe@gmail.com>
2018-11-02 23:28:51 +0800

    update minimum-index-sum-of-two-lists.py

--- Python/minimum-index-sum-of-two-lists.py
+++ Python/minimum-index-sum-of-two-lists.py
@@ -5,4 +5,19 @@ class Solution:
         :type list2: List[str]
         :rtype: List[str]
         """
-        
+        lookup = {}
+        for i, s in enumerate(list1):
+            lookup[s] = i
+
+        res = []
+        minSum = float("inf")
+
+        for j, s in enumerate(list2):
+            if s in lookup:
+                if j + lookup[s] < minSum:
+                    res = [s]
+                    minSum = j + lookup[s]
+                elif j + lookup[s] == minSum:
+                    res.append(s)
+
+        return res
