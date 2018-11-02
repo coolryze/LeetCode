4e055dd0b20872417072e6578420af9d6e65de3b
heyuze <ryzehe@gmail.com>
2018-11-02 23:26:24 +0800

    update minimum-index-sum-of-two-lists.swift

--- Swift/minimum-index-sum-of-two-lists.swift
+++ Swift/minimum-index-sum-of-two-lists.swift
@@ -1,5 +1,24 @@
 class Solution {
     func findRestaurant(_ list1: [String], _ list2: [String]) -> [String] {
-        
+        var lookup = [String: Int]()
+        for (i, str) in list1.enumerated() {
+            lookup[str] = i
+        }
+
+        var res = [String]()
+        var minSum = Int.max
+
+        for (j, str) in list2.enumerated() {
+            if let i = lookup[str] {
+                if i + j < minSum {
+                    res = [str]
+                    minSum = i + j
+                } else if i + j == minSum {
+                    res.append(str)
+                }
+            }
+        }
+
+        return res
     }
 }
