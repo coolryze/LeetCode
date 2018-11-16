b99db17d59aad9d433c25586c120b9a36cff1414
heyuze <ryzehe@gmail.com>
2018-11-16 15:10:01 +0800

    add binary-number-with-alternating-bits.py

---
+++ Python/binary-number-with-alternating-bits.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def hasAlternatingBits(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        n, curr = divmod(n, 2)
+
+        while n > 0:
+            if curr == n % 2:
+                return False
+            n, curr = divmod(n, 2)
+
+        return True
