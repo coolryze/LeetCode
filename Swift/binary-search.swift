a1124ac960454beaf986682ccbb2b75e178a14c8
heyuze <ryzehe@gmail.com>
2018-11-20 20:02:56 +0800

    add binary-search.swift

---
+++ Swift/binary-search.swift
@@ -0,0 +1,19 @@
+class Solution {
+    func search(_ nums: [Int], _ target: Int) -> Int {
+        var left = 0
+        var right = nums.count - 1
+
+        while left <= right {
+            let mid = left + (right - left) / 2
+            if nums[mid] > target {
+                right = mid - 1
+            } else if nums[mid] < target {
+                left = mid + 1
+            } else {
+                return mid
+            }
+        }
+
+        return -1
+    }
+}
