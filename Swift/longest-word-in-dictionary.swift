512ccc256c0cc5dcae815399adcf7d722b0892ba
heyuze <ryzehe@gmail.com>
2018-11-26 20:09:13 +0800

    update longest-word-in-dictionary.swift

--- Swift/longest-word-in-dictionary.swift
+++ Swift/longest-word-in-dictionary.swift
@@ -1,5 +1,20 @@
 class Solution {
     func longestWord(_ words: [String]) -> String {
+        let sortedWords = words.sorted()
+        var set = Set([""])
+        var res = ""
 
+        print(sortedWords)
+
+        for word in sortedWords {
+            if set.contains(String(word.prefix(word.count-1))) {
+                set.insert(word)
+                if word.count > res.count {
+                    res = word
+                }
+            }
+        }
+
+        return res
     }
 }
