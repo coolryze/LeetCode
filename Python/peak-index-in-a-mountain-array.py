bb863161660c91a18d1b5ca07245c75e4edb2f0d
heyuze <ryzehe@gmail.com>
2019-01-16 20:11:02 +0800

    add peak-index-in-a-mountain-array.py

---
+++ Python/peak-index-in-a-mountain-array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def peakIndexInMountainArray(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        left, right = 0, len(A)
+
+        while left < right:
+            mid = left + (right-left)//2
+            if A[mid] > A[mid+1]:
+                right = mid
+            else:
+                left = mid+1
+
+        return left
