5a14558abbca8bec30553a3faba244a84cb3f68a
heyuze <ryzehe@gmail.com>
2019-01-19 21:03:28 +0800

    add lemonade-change.swift

---
+++ Swift/lemonade-change.swift
@@ -0,0 +1,29 @@
+class Solution {
+    func lemonadeChange(_ bills: [Int]) -> Bool {
+        var five = 0
+        var ten = 0
+
+        for bill in bills {
+            if bill == 5 {
+                five += 1
+            } else if bill == 10 {
+                if five == 0 {
+                    return false
+                }
+                five -= 1
+                ten += 1
+            } else {
+                if ten > 0 && five > 0 {
+                    ten -= 1
+                    five -= 1
+                } else if five >= 3 {
+                    five -= 3
+                } else {
+                    return false
+                }
+            }
+        }
+
+        return true
+    }
+}
