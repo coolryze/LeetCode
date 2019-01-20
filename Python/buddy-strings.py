51b3ee37de7195916452d320891404236e2fa95b
heyuze <ryzehe@gmail.com>
2019-01-20 18:36:39 +0800

    add lemonade-change.py

--- Python/buddy-strings.py
+++ Python/buddy-strings.py
@@ -5,4 +5,16 @@ class Solution:
         :type B: str
         :rtype: bool
         """
-        
+        if len(A) != len(B):
+            return False
+
+        diff = []
+
+        for a, b in zip(A, B):
+            if a != b:
+                diff.append((a, b))
+                if len(diff) > 2:
+                    return False
+
+        return (not diff and len(set(A)) < len(A)) or \
+               (len(diff) == 2 and diff[0] == diff[1][::-1])
