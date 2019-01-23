836390574aea833ecca1b096921343382a4a7d75
heyuze <ryzehe@gmail.com>
2019-01-23 21:48:32 +0800

    add middle-of-the-linked-list.swift

---
+++ Swift/middle-of-the-linked-list.swift
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.next = nil
+ *     }
+ * }
+ */
+class Solution {
+    func middleNode(_ head: ListNode?) -> ListNode? {
+        var slow = head
+        var fast = head
+
+        while fast != nil && fast?.next != nil {
+            slow = slow?.next
+            fast = fast?.next?.next
+        }
+
+        return slow
+    }
+}
