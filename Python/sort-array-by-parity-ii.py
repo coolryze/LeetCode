a112b2681a132e5028de812169e714fda04b256d
heyuze <ryzehe@gmail.com>
2019-02-20 23:47:09 +0800

    add sort-array-by-parity-ii.py

---
+++ Python/sort-array-by-parity-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def sortArrayByParityII(self, A):
+        """
+        :type A: List[int]
+        :rtype: List[int]
+        """
+        j = 1
+
+        for i in range(0, len(A), 2):
+            if A[i] % 2:
+                while A[j] % 2:
+                    j += 2
+                A[i], A[j] = A[j], A[i]
+
+        return A
