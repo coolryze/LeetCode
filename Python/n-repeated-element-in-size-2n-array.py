8b1e01453bc55f4b1c62bd12e82599a9cb8a27a3
heyuze <ryzehe@gmail.com>
2019-03-11 20:05:30 +0800

    add n-repeated-element-in-size-2n-array.py

---
+++ Python/n-repeated-element-in-size-2n-array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def repeatedNTimes(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        for i in range(2, len(A)):
+            if A[i - 1] == A[i] or A[i - 2] == A[i]:
+                return A[i]
+
+        return A[0]
