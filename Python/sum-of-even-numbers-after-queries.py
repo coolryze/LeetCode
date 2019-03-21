30eead189b719d4ea5ef87bb21d1886bfceacb23
heyuze <ryzehe@gmail.com>
2019-03-21 19:37:38 +0800

    add sum-of-even-numbers-after-queries.py

---
+++ Python/sum-of-even-numbers-after-queries.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sumEvenAfterQueries(self, A, queries):
+        """
+        :type A: List[int]
+        :type queries: List[List[int]]
+        :rtype: List[int]
+        """
+        
